88bb9950b7ed1995d9d6df62232a89e01ff91223
Andrew Vos <andrew.vos@gmail.com>
2011-08-16 11:48:33 +0100

    extract config so I can publish this project online

--- application.rb
+++ application.rb
@@ -3,12 +3,20 @@ unless File.exist?("certificate.pem")
   exit(1)
 end
 
+unless File.exist?("configuration.rb")
+  puts "Configuration file does not exist. It should be called 'configuration.rb' and should contain the following contents:"
+  puts <<-CONFIG
+  set :hudson, "https://ci-pal.int.bbc.co.uk/hudson/"
+  set :project, "News"
+  CONFIG
+end
+
 require "json"
 require "sinatra"
+require File.join(File.dirname(__FILE__), "configuration")
 
 get "/" do
-  project = "News"
-  url = "https://ci-pal.int.bbc.co.uk/hudson/view/#{project}/api/json"
+  url = "#{settings.hudson}/view/#{settings.project}/api/json"
   response = `curl #{url} --cert certificate.pem --insecure`
   json = JSON.parse(response)
   jobs = json["jobs"].map do |job|
