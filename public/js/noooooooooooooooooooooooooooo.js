6bbdfbd535004836d5bc7437544aab853ba68113
Matt Chadburn <matt@commuterjoy.co.uk>
2011-08-23 16:56:54 +0100

    added threshold so sound only plays once every ~30 mins

--- public/js/noooooooooooooooooooooooooooo.js
+++ public/js/noooooooooooooooooooooooooooo.js
@@ -1,7 +1,7 @@
     if (document.querySelector){
         // play some audio when we detect a red build
         var el = document.querySelector(".red");
-    	if (el) {
+        if (el && (Math.random() * 100 >> 0) === 1) { // only play sound every once in a while, as it's fairly annoying ~ once every 30 minutes
             document.getElementById('audio').innerHTML = '<audio src="/mp3/noooooooooooooooo.mp3?" id="nooooo">Your browser does not support the <code>audio</code> element.</audio>';
             document.getElementById("nooooo").play();
         }   
