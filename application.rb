0d9dd13dd327af108af526281f82a3c1d4615eb5
David White <david@spry-soft.com>
2011-12-02 12:55:17 +0000

    Updated main get to use Ruby net/http standard lib over unix curl

--- application.rb
+++ application.rb
@@ -8,6 +8,10 @@ unless File.exist?("configuration.rb")
   puts <<-CONFIG
   set :hudson, "https://your_server/hudson"
   set :project, "News"
+
+  # if behind a proxy server
+  set :proxy_address, "your_proxy_address"
+  set :proxy_port, 80
   CONFIG
 end
 
@@ -19,11 +23,23 @@ require File.join(File.dirname(__FILE__), "configuration")
 get "/" do
   url = "#{settings.hudson}/view/#{settings.project}/api/json"
 
-  curl = "curl #{url} --cert certificate.pem --insecure"
-  curl << " -x #{ENV['http_proxy']}" if (ENV["http_proxy"])
-  json = `#{curl}`
+  if settings.respond_to? :proxy_address
+    http = Net::HTTP::Proxy(settings.proxy_address, settings.proxy_port).new(uri.host, uri.port)
+  else
+    http = Net::HTTP.new(uri.host, uri.port)
+  end
+  
+  pem = File.read File.join(File.dirname(__FILE__), "certificate.pem")
+  http.use_ssl     = true
+  http.cert        = OpenSSL::X509::Certificate.new pem
+  http.key         = OpenSSL::PKey::RSA.new pem
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-  jobs = get_jobs_from_json json
+  request  = Net::HTTP::Get.new uri.request_uri
+  response = http.request request
+
+  jobs = get_jobs_from_json response.body
+
   haml :index, :locals => {:jobs => jobs, :settings => settings}
 end
 
